84ac6433cd9a5fe34ec1be1791fec86cca2119dd
yamasita <yamasita@yaasita.net>
2016-05-02 02:38:37 +0900

    update

--- bin/004_post_message.pl
+++ bin/004_post_message.pl
@@ -9,7 +9,7 @@ use Encode;
 use utf8;
 binmode STDOUT, ":utf8";
 
-my $slack = Slack->new("a");
+my $slack_a = Slack->new("a");
 
 my @files = <tmp/a_history_*>;
 for (@files){
@@ -27,9 +27,9 @@ sub post_message {
         $json = decode_json($data);
     }
     for(sort {$a->{ts} <=> $b->{ts}} @{$json->{messages}}){
-        print $slack->id_to_user_name($_->{user}) . ": ";
+        print $slack_a->id_to_user_name($_->{user}) . ": ";
         my $text = $_->{text};
-        $text =~ s/<\@([A-Z0-9]+)>/'"@" . $slack->id_to_user_name("' . $1 . '")'/eeg;
+        $text =~ s/<\@([A-Z0-9]+)>/'"@" . $slack_a->id_to_user_name("' . $1 . '")'/eeg;
         print $text;
         say "";
     }
